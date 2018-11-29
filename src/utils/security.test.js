30435146b4b48a77dd0d5aff7749f1a7244c35c6
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2018-11-29 17:59:03 +0500

    Tests updates

--- src/utils/security.test.js
+++ src/utils/security.test.js
@@ -1,21 +1,56 @@
-//import { expect } from "chai";
+import { expect } from "chai";
 import { getPermissions } from "./security";
 
 describe("Security tests", () => {
 
     it("No permission for specified roles", () => {
         const result = getPermissions(["USER", "MANAGER"], { "ADMIN": { read: true, modify: true } });
-        //expect(result).to.be.deep.equal({});
+        expect(result).to.be.deep.equal({ });
     });
 
-    it("Single role", () => {
-
-        //const schema = { "ADMIN": { read: { entries: res => 1, fields: res => 1 } } };
-
+    it("Single role", () => {        
+        const result = getPermissions(["USER"], { "USER": { read: true, modify: true } });
+        expect(result).to.be.deep.equal({ read: true, modify: true });
     });
 
-    it("Mixing roles with inclusive exclusive fields", () => {
+    it("Mixed roles ", () => {
+        const schema = {
+            "USER": { read: { entries: user => ({ user }), fields: "field" }, modify: true },
+            "MODERATOR": { read: true, modify: { fields: "field" } }
+        };   
+        const result = getPermissions(["USER", "MODERATOR"], schema);
+        expect(result).to.be.deep.equal({ read: true, modify: true });
+    });
 
+    it("With 'ALL' role and without required one", () => {
+        const schema = {
+            "ALL": { read: true, modify: true },
+            "MODERATOR": { read: { entries: () => {} }, modify: { fields: "field" } }
+        };   
+        const result = getPermissions(["USER", "MODERATOR"], schema);
+        expect(result).to.be.deep.equal({ read: true, modify: true });
+    });
+
+    // it("With 'ALL' role and required one", () => {
+    //     const schema = {
+    //         "ALL": { read: true, modify: true },
+    //         "MODERATOR": { read: { entries: () => {} }, modify: { fields: "field" } }
+    //     };   
+    //     const result = getPermissions(["USER", "MODERATOR"], schema);
+    //     expect(result).to.be.deep.equal({ read: true, modify: true });
+    // });
+
+    it("Mixing roles with inclusive and exclusive fields", () => {
+        
+    });
+
+    it("Mixing roles with different functions", () => {
+        const schema = {
+            "USER": { read: { entries: user => ({ user: "user" }) } },
+            "MODERATOR": { read: { entries: user => ({ department: "department" }) } }
+        };   
+        const result = getPermissions(["USER", "MODERATOR"], schema);
+        expect(result).to.deep.equal({ read: { entries: [ { user: "user" }, { department: "department" } ] } });
     });
 
 });
