e7b6280a703c94fa6a0102a2b0029fba35db576e
Vadim <contact.vadimsartakov@gmail.com>
2018-12-12 23:38:51 +0500

    Validate tests draft

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,26 +1,28 @@
-const validateRecursively = (object, rootProperty, validationSchema) => {
+const validateRecursively = (object, rootProperty, validationSchema, sourceObject) => {
     
-    
+    return Object.keys(object).reduce((prev, property) => {
 
-    const objectValue = object[property];
+        const fullProperty = `${rootProperty ? rootProperty + "." : ""}${property}`;
+        const value = object[property];
+        const validators = validationSchema[property];
 
-    let result;
+        const validationResult = validators.reduce((prev, validator) => {
+            const isValid = validator(value, fullProperty, sourceObject);
+        }, {});
 
-    if (Array.isArray(object)) {
+        if (Array.isArray(value)) {
+            value.forEach(item => validateRecursively(object, ));
+        } else if (typeof(value) === "object") {
 
-    } else if () {
+        }
 
-    }
-
-    result = Object.keys(object).reduce((prev, property) => {
         return validateRecursively(object[property], validationSchema[property], validationSchema);
-    }, {});
 
-    return result;
+    }, {});
 
 };
 
-export const validate = (object, validationSchema) => {
-    const errors = validateRecursively(object, "", validationSchema);
+export const validate = (object, schema) => {
+    const errors = validateRecursively(object, "", schema, object);
     return errors;
 };
