63c36df47fa314d5834b925a916d24f9a725b2fa
Vadim Sartakov <contact.vadimsartakov@gmail.com>
2018-12-26 10:36:16 +0500

    Min max array unique validators

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,14 +1,42 @@
+import { format } from "util";
+import _ from "lodash";
+
 export const required = message => value => {
     if (value === undefined ||
             value === null ||
-            (value.length !== undefined && value.length === 0) ||
+            value.length === 0 ||
             (typeof(value) === "object" && Object.keys(value).length === 0)) return (message || "Value is required");
     return undefined;
 };
 
 export const match = (pattern, message) => value => {
-    const regexp = new RegExp(pattern);
-    return regexp.test(value) ? undefined : (message || "Value is not valid");
+    if (value == undefined || value == null) return;
+    return new RegExp(pattern).test(value) ? undefined : (message || "Value is not valid");
+};
+
+const getValueToCheckBound = value => ( ( typeof(value) === "string" || isNaN(value) ) && value.length ) || value;
+
+export const min = (minValue, message) => value => {
+    if (value == undefined || value == null) return;
+    const valueToCheck = getValueToCheckBound(value);
+    return valueToCheck < minValue ? (message || format("Should be at least %s", minValue)) : undefined;
+};
+
+export const max = (maxValue, message) => value => {
+    if (value == undefined || value == null) return;
+    const valueToCheck = getValueToCheckBound(value);
+    return valueToCheck > maxValue ? (message || format("Should be not more than %s", maxValue)) : undefined;
+};
+
+export const unique = (message, comparator = (x, y) => x === y) => (value, path, allValues) => {
+    if (value == undefined || value == null) return;
+    // Removing index from path
+    const arrayPath = path.replace(/\[\d+\]$/, "");
+    const items = _.get(allValues, arrayPath);
+    const occurrences = items.reduce((prev, cur) => {
+        return comparator(cur, value) ? prev + 1 : prev;
+    }, 0);
+    return occurrences > 1 ? (message || "Value is not unique") : undefined;
 };
 
 const validateRecursively = (object, rootProperty, validationSchema, sourceObject) => {
--- src/utils/validator.test.js
+++ src/utils/validator.test.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { validate, required, match } from "./validator";
+import { validate, required, match, min, max, unique } from "./validator";
 
 describe("Validator", () => {
 
@@ -63,6 +63,103 @@ describe("Validator", () => {
             it("Fail with custom message", () => {
                 const validator = match(/w+/, "Custom message");
                 expect(validator("-+*")).to.equal("Custom message");
+            });
+
+        });
+
+        describe("Min", () => {
+
+            it("No value", () => {
+                const validator = min(5);
+                expect(validator()).not.to.be.ok;
+            });
+
+            it("Valid", () => {
+                const validator = min(5);
+                expect(validator(8)).not.to.be.ok;
+                expect(validator(8.45)).not.to.be.ok;
+                expect(validator([1, 2, 3, 4, 5])).not.to.be.ok;
+                expect(validator("abcdefghi")).not.to.be.ok;
+                expect(validator("123456789")).not.to.be.ok;
+            });
+
+            it("Invalid", () => {
+                const validator = min(5);
+                const error = "Should be at least 5";
+                expect(validator(2)).to.equal(error);
+                expect(validator(2.45)).to.equal(error);
+                expect(validator([1, 2])).to.equal(error);
+                expect(validator("ab")).to.equal(error);
+                expect(validator("12")).to.equal(error);
+            });
+
+            it("Custom message", () => {
+                const error = "Custom";
+                const validator = min(5, error);
+                expect(validator(2)).to.equal(error);
+                expect(validator(2.45)).to.equal(error);
+                expect(validator([1, 2])).to.equal(error);
+                expect(validator("ab")).to.equal(error);
+            });
+
+        });
+
+        describe("Max", () => {
+
+            it("Valid", () => {
+                const validator = max(5);
+                expect(validator(2)).not.to.be.ok;
+                expect(validator(2.45)).not.to.be.ok;
+                expect(validator([1, 2])).not.to.be.ok;
+                expect(validator("ab")).not.to.be.ok;
+                expect(validator("12")).not.to.be.ok;
+            });
+
+            it("Invalid", () => {
+                const error = "Should be not more than 5";
+                const validator = max(5);
+                expect(validator(8)).to.equal(error);
+                expect(validator(8.45)).to.equal(error);
+                expect(validator([1, 2, 3, 4, 5, 6])).to.equal(error);
+                expect(validator("abcdefghi")).to.equal(error);
+                expect(validator("123456789")).to.equal(error);
+            });
+
+            it("Custom message", () => {
+                const error = "Custom";
+                const validator = max(5, "Custom");
+                expect(validator(8)).to.equal(error);
+                expect(validator(8.45)).to.equal(error);
+                expect(validator([1, 2, 3, 4, 5, 6])).to.equal(error);
+                expect(validator("abcdefghi")).to.equal(error);
+                expect(validator("123456789")).to.equal(error);
+            });
+
+        });
+
+        describe("Unique", () => {
+
+            it("Valid", () => {
+                const validator = unique();
+                expect(validator("1", "array[0]", { array: ["1", "2"] })).not.to.be.ok;
+            });
+
+            it("Invalid", () => {
+                const validator = unique();
+                expect(validator("1", "array[0]", { array: ["1", "2", "1", "2"] })).to.equal("Value is not unique");
+                expect(validator("2", "array[0]", { array: ["1", "2", "1", "2"] })).to.equal("Value is not unique");
+            });
+
+            it("Mixed", () => {
+                const validator = unique();
+                expect(validator("1", "array[0]", { array: ["1", "2", "1", "2"] })).to.equal("Value is not unique");
+                expect(validator("3", "array[0]", { array: ["1", "2", "1", "2"] })).not.to.be.ok;
+            });
+
+            it("Custom message and comparator", () => {
+                const validator = unique("Custom message", (x, y) => x.id === y.id);
+                expect(validator({ id: "1"}, "array[0]", { array: [{ id: "1"} , { id: "2" }] })).not.to.be.ok;
+                expect(validator({ id: "1"}, "array[0]", { array: [{ id: "1"} , { id: "2" }, { id: "1"}] })).to.equal("Custom message");
             });
 
         });
